632e69e48785b50fc17714a9016a45a2c2f1cf83
nikhil <dropboxnikspath@gmail.com>
2023-06-19 04:41:06 -0700

    aws db

--- core/settings.py
+++ core/settings.py
@@ -74,26 +74,26 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'django-first',
-#         'USER': 'admin',
-#         'PASSWORD': 'rXnjA6srnQ3bNIWWEZrK',
-#         'HOST': 'django-first.c2dijzzaib5q.ap-south-1.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
-#         'PORT': '3306',
-#         'OPTIONS': {
-#         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#     },
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django-first',
+        'USER': 'admin',
+        'PASSWORD': 'rXnjA6srnQ3bNIWWEZrK',
+        'HOST': 'django-first.c2dijzzaib5q.ap-south-1.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
+    }
+}
 # Server: sql12.freemysqlhosting.net
 # Name: sql12627207
 # Username: sql12627207
