53dafd6ef5c5f596fb8cd79c6d7b63775bb9a6c3
nikhil <dropboxnikspath@gmail.com>
2023-06-19 04:22:21 -0700

    requiremt

--- home/models.py
+++ home/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title=models.CharField(max_length=200)
+    title=models.CharField(max_length=200,default="test")
 
     def __str__(self) -> str:
         return self.title
